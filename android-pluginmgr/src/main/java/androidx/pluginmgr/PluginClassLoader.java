36c4d3caed9e4de3944cb87688d42208d3cec574
houkx <1084940623@qq.com>
2014-12-14 17:17:38 +0800

    从master同步bugfix: 自定义view不能加载的问题

--- android-pluginmgr/src/main/java/androidx/pluginmgr/PluginClassLoader.java
+++ android-pluginmgr/src/main/java/androidx/pluginmgr/PluginClassLoader.java
@@ -66,4 +66,49 @@ class PluginClassLoader extends DexClassLoader {
 		return actLoader.loadClass(ActivityOverider.targetClassName);
 	}
 	
+	protected Object getClassLoadingLock(String name){
+		return name;
+	}
+
+	protected Class<?> loadClass(String name, boolean resolve)
+			throws ClassNotFoundException {
+		synchronized (getClassLoadingLock(name)) {
+			// First, check if the class has already been loaded
+			Class<?> c = findLoadedClass(name);
+			if (c == null) {
+//				long t0 = System.nanoTime();
+				try {
+					ClassLoader parent = getParent();
+					if (parent != null) {
+						if (parent.getClass() == FrameworkClassLoader.class) {
+							parent = parent.getParent();
+						}
+						if (parent != null) {
+							c = parent.loadClass(name);
+						}
+					}
+				} catch (ClassNotFoundException e) {
+					// ClassNotFoundException thrown if class not found
+					// from the non-null parent class loader
+				}
+
+				if (c == null) {
+					// If still not found, then invoke findClass in order
+					// to find the class.
+					// long t1 = System.nanoTime();
+					c = findClass(name);
+					//
+					// // this is the defining class loader; record the stats
+					// sun.misc.PerfCounter.getParentDelegationTime().addTime(t1
+					// - t0);
+					// sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+					// sun.misc.PerfCounter.getFindClasses().increment();
+				}
+			}
+			if (resolve) {
+				resolveClass(c);
+			}
+			return c;
+		}
+	}
 }
